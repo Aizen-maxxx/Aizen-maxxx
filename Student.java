2ccd3bfa1830d4b3c421079eacdf467e082edbe9
Aizen-maxxx <noobthegamer2324@gmail.com>
2025-11-30 23:32:54 +0800

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,26 +1,105 @@
 
-package school.management.system;
+package studentinfosystem;
 
+public class Student implements Display {
 
-class Student {
-    String name, gender, contact, email, course;
-    int id, age, year;
+    String name;
+    String studentId;
+    String gender;
+    int age;
+    String contactNumber;
+    String email;
+    int yearLevel;
+    String course;
 
-    public Student(String name, int id, String gender, int age, String contact,
-            String email, int year, String course) {
-
+    Student(String name, String studentId, String gender, int age, String contactNumber,
+            String email, int yearLevel, String course) {
         this.name = name;
-        this.id = id;
+        this.studentId = studentId;
         this.gender = gender;
         this.age = age;
-        this.contact = contact;
+        this.contactNumber = contactNumber;
         this.email = email;
-        this.year = year;
+        this.yearLevel = yearLevel;
         this.course = course;
     }
-
-    public void display() {
-        System.out.printf("%-10s %-10d %-8s %-5d %-10s %-22s %-6d %-10s\n",
-                name, id, gender, age, contact, email, year, course);
+    
+    
+     public void setStudentId(String v) {
+        studentId = v;
     }
+
+    public void setGender(String v) {
+        gender = v;
+    }
+
+    public void setAge(int v) {
+        age = v;
+    }
+
+    public void setContactNumber(String v) {
+        contactNumber = v;
+    }
+
+    public void setEmail(String v) {
+        email = v;
+    }
+
+    public void setYearLevel(int v) {
+        yearLevel = v;
+    }
+
+    public void setCourse(String v) {
+        course = v;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getYearLevel() {
+        return yearLevel;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void updateInfo(String newCourse, int newYearLevel) {
+        this.course = newCourse;
+        this.yearLevel = newYearLevel;
+    }
+
+    public String getInfo() {
+        return "ID: " + studentId + ", Gender: " + gender + ", Age: " + age
+                + ", Contact: " + contactNumber + ", Email: " + email
+                + ", Year Level: " + yearLevel + ", Course: " + course;
+    }
+
+    @Override
+    public void Display() {
+        System.out.println(getInfo());
+
+    }
+
 }
